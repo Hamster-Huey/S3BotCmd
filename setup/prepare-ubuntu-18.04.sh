4377cfb26b44675d27fe953e6a7a669bcff44620
James Christianson <jamesrchristianson@gmail.com>
2018-12-19 23:44:43 -0800

    Update prepare-ubuntu-18.04.sh

--- setup/prepare-ubuntu-18.04.sh
+++ setup/prepare-ubuntu-18.04.sh
@@ -6,4 +6,5 @@ sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 sudo apt-get install apt-transport-https -y
 sudo apt-get update 
 sudo apt-get install dotnet-sdk-2.2 -y
-
+sudo apt-get install docker.io -y
+sudo apt-get install docker-compose -y
